53c457abfc38138902171b0273c276a24ad5fec9
joaa17038 <ninjaleopard@outlook.com>
2021-04-12 05:48:48 +0200

    fix linked files

--- slidingWindowMaximum.vhd
+++ slidingWindowMaximum.vhd
@@ -1,1 +1,68 @@
-/Users/shiju/Documents/FPGA/data/project_1/project_1.srcs/sources_1/new/slidingWindowMaximum.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+
+entity slidingWindowMaximum is
+generic (WINDOWSIZE, DATAWIDTH: Integer);
+port (
+    -- Inputs
+    clk : in std_logic;
+    rst : in std_logic;
+    inputNumber : in std_logic_vector(DATAWIDTH-1 downto 0);
+    -- Outputs
+    max : out std_logic_vector(DATAWIDTH-1 downto 0) := (others => '0');
+    maxValid : out std_logic := '0');
+end entity;
+
+
+architecture rtl of slidingWindowMaximum is
+
+    signal countInput : integer range 0 to WINDOWSIZE-1 := 0;
+    signal internalValid : std_logic;
+
+begin
+
+    process (clk)
+
+        variable tempMax : std_logic_vector(DATAWIDTH-1 downto 0) := (others => '0');
+        type mem is array(WINDOWSIZE-1 downto 0) of std_logic_vector(DATAWIDTH-1 downto 0);
+        variable memory : mem;
+
+    begin
+
+        if rising_edge(clk) then
+
+            if rst = '1' then
+                internalValid <= '0';
+                max <= (others => '0');
+                maxValid <= '0';
+            else
+                memory := memory(WINDOWSIZE-2 downto 0) & inputNumber;
+
+                if countInput = WINDOWSIZE then
+                    internalValid <= '1';
+                    for i in 0 to WINDOWSIZE-1 loop
+                        if memory(i) > tempMax then
+                            tempMax := memory(i);
+                        end if;
+                    end loop;
+                else
+                    countInput <= countInput + 1;
+                end if;
+
+                if internalValid = '1' and inputNumber > tempMax then
+                    tempMax := inputNumber;
+                end if;
+
+                max <= tempMax;
+                maxValid <= internalValid;
+            end if;
+
+        end if;
+
+    end process;
+
+end architecture;
--- testbench.vhd
+++ testbench.vhd
@@ -1,1 +1,78 @@
-/Users/shiju/Documents/FPGA/data/testbench.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+
+entity testbench is
+end entity;
+
+architecture sim of testbench is
+
+    constant WINDOWSIZE : integer := 4;
+    constant DATAWIDTH : integer := 8;
+
+    signal clk : std_logic := '1';
+    signal rst: std_logic := '1';
+    signal inputNumber : std_logic_vector(DATAWIDTH-1 downto 0) := (others => '0');
+
+    signal max : std_logic_vector(DATAWIDTH-1 downto 0);
+    signal ones : std_logic_vector(DATAWIDTH-1 downto 0);
+    signal maxValid : std_logic;
+
+begin
+
+    process begin
+        clk <= not clk;
+        wait for 10ns;
+    end process;
+
+    process begin
+        wait for 40ns;
+        rst <= '0';
+    end process;
+
+    process
+
+        file read_file : text is in "./simulation.in";
+        variable line_v : line;
+        variable input_from_file : std_logic_vector(DATAWIDTH-1 downto 0);
+
+    begin
+
+        wait until clk'event and clk='1';
+
+        if rst = '1' then
+            inputNumber <= (others => '0');
+        else
+            if not endfile(read_file) then
+                readline(read_file, line_v);
+                hread(line_v, input_from_file);
+                inputNumber <= input_from_file;
+            end if;
+        end if;
+
+    end process;
+
+    i_SlidingWindowMaximum1 : entity work.slidingWindowMaximum(rtl)
+    generic map (
+        WINDOWSIZE => WINDOWSIZE,
+        DATAWIDTH => DATAWIDTH)
+    port map (
+        clk => clk,
+        rst => rst,
+        inputNumber => inputNumber,
+        max => max,
+        maxValid => maxValid);
+
+    i_CountBits1 : entity work.countBits(rtl)
+    generic map (
+        DATAWIDTH => DATAWIDTH)
+    port map (
+        clk => clk,
+        rst => rst,
+        inputNumber => inputNumber,
+        ones => ones);
+
+end architecture;
