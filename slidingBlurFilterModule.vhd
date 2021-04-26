9e0cc677a91ce3f6d7ccfc37156f3e8779ded8dd
joaa17038 <ninjaleopard@outlook.com>
2021-04-26 15:01:25 +0200

    fix port map

--- slidingBlurFilterModule.vhd
+++ slidingBlurFilterModule.vhd
@@ -27,18 +27,16 @@ architecture rtl of slidingBlurFilterV1 is
 
     type typeState is (RECEIVE, FILTER);
     type threeRows is array(0 to 2) of std_logic_vector(PIXELSIZE*PACKETSIZE-1 downto 0);
-    
+
     constant ZERO : std_logic_vector(PIXELSIZE*PACKETSIZE-1 downto 0) := (others => '0');
 
     signal state : typeState := RECEIVE;
     signal buffered : threeRows := (others => (others => '0'));
-    
-    signal IMAGEHEIGHT : unsigned(11 downto 0) := (others => '0');
-    signal IMAGEWIDTH : unsigned(11 downto 0) := (others => '0');
-    
+
     signal heightPointer : unsigned(11 downto 0) := (others => '0');
     signal widthPointer : unsigned(11 downto 0) := (others => '0');
-    
+    signal IMAGEHEIGHT : unsigned(11 downto 0) := (others => '0');
+    signal IMAGEWIDTH : unsigned(11 downto 0) := (others => '0');
     signal divisorSide : unsigned(3 downto 0) := (others => '0');
     signal divisorMiddle : unsigned(3 downto 0) := (others => '0');
 
@@ -49,30 +47,28 @@ begin
         if rising_edge(clk) then
 
             if rst = '1' then
-                state <= RECEIVE;
-                
                 s_axi_ready <= '1';
                 m_axi_last <= '0';
                 m_axi_valid <= '0';
                 m_axi_data <= (others => '0');
-                
-                IMAGEHEIGHT <= (others => '0');
-                IMAGEWIDTH <= (others => '0');
-                
+
+                state <= RECEIVE;
+                buffered <= (others => (others =>'0'));
                 heightPointer <= (others => '0');
                 widthPointer <= (others => '0');
-                buffered <= (others => (others =>'0'));
-                
+                IMAGEHEIGHT <= (others => '0');
+                IMAGEWIDTH <= (others => '0');
                 divisorSide <= X"4";
                 divisorMiddle <= X"6";
 
-            else          
+            else
                 if m_axi_ready = '1' then
                     m_axi_last <= '0';
                     m_axi_valid <= '0';
-                
+
                     case state is
                         when RECEIVE =>
+                            -- Buffer first two rows
                             if s_axi_valid = '1' then
                                 buffered <= buffered(1 to 2) & s_axi_data;
                                 IMAGEHEIGHT <= unsigned(s_axi_dim(23 downto 12));
@@ -82,21 +78,19 @@ begin
                                     heightPointer <= (others => '0');
                                     state <= FILTER;
                                 end if;
+                            -- Prepare for next image
                             elsif heightPointer = IMAGEHEIGHT then
                                 s_axi_ready <= '1';
                                 m_axi_data <= (others => '0');
-                                
+                                buffered <= (others => (others =>'0'));
                                 heightPointer <= (others => '0');
                                 widthPointer <= (others => '0');
-                                buffered <= (others => (others =>'0'));
-                                
                                 IMAGEHEIGHT <= (others => '0');
                                 IMAGEWIDTH <= (others => '0');
-                                
                                 divisorSide <= X"4";
                                 divisorMiddle <= X"6";
                             end if;
-    
+
                         when FILTER =>
                             if s_axi_valid = '1' or heightPointer = IMAGEHEIGHT-1 or heightPointer = IMAGEHEIGHT-2 then
                                 -- Leftside pixel 2x2, 2x3
@@ -107,7 +101,7 @@ begin
                                       + unsigned(buffered(1)(PIXELSIZE*2-1 downto PIXELSIZE))
                                       + unsigned(buffered(2)(PIXELSIZE-1 downto 0))
                                       + unsigned(buffered(2)(PIXELSIZE*2-1 downto PIXELSIZE))) / divisorSide, PIXELSIZE));
-    
+
                                 -- Middle pixel 2x3, 3x3
                                 for i in 2 to PACKETSIZE-1 loop
                                     m_axi_data(PIXELSIZE*i-1 downto PIXELSIZE*i-PIXELSIZE) <= std_logic_vector(resize((
@@ -121,7 +115,7 @@ begin
                                           + unsigned(buffered(2)(PIXELSIZE*(i)-1 downto PIXELSIZE*(i)-PIXELSIZE))
                                           + unsigned(buffered(2)(PIXELSIZE*(i+1)-1 downto PIXELSIZE*(i+1)-PIXELSIZE))) / divisorMiddle, PIXELSIZE));
                                 end loop;
-    
+
                                 -- Rightside pixel 2x2, 2x3
                                 m_axi_data(PIXELSIZE*PACKETSIZE-1 downto PIXELSIZE*PACKETSIZE-PIXELSIZE) <= std_logic_vector(resize((
                                         resize(unsigned(buffered(0)(PIXELSIZE*(PACKETSIZE-1)-1 downto PIXELSIZE*(PACKETSIZE-1)-PIXELSIZE)), PIXELSIZE*2-1)
@@ -136,16 +130,16 @@ begin
                                 else
                                     buffered <= buffered(1 to 2) & ZERO;
                                 end if;
-                                
+
                                 heightPointer <= heightPointer + 1;
                                 m_axi_valid <= '1';
                             end if;
-                            
+
                             -- Not ready for more input
                             if s_axi_last = '1' then
                                 s_axi_ready <= '0';
                             end if;
-    
+
                             -- Change divisors
                             if heightPointer = 0 then
                                 divisorSide <= X"6";
@@ -160,7 +154,7 @@ begin
                             end if;
 
                     end case;
-                    
+
                 end if;
 
             end if;
--- slidingBlurFilterTb.vhd
+++ slidingBlurFilterTb.vhd
@@ -17,17 +17,17 @@ architecture sim of testbench is
     signal clk : std_logic := '1';
     signal rst: std_logic := '1';
 
-    signal m_axi_data : std_logic_vector(PACKETSIZE*PIXELSIZE-1 downto 0) := (others => '0');
-    signal m_axi_dim : std_logic_vector(23 downto 0) := (others => '0');
+    signal sim_data : std_logic_vector(PACKETSIZE*PIXELSIZE-1 downto 0) := (others => '0');
+    signal sim_dim : std_logic_vector(23 downto 0) := (others => '0');
     signal assertRow : std_logic_vector(PACKETSIZE*PIXELSIZE-1 downto 0) := (others => '0');
-    signal m_axi_valid: std_logic := '0';
-    signal m_axi_ready: std_logic;
-    signal m_axi_last: std_logic := '0';
+    signal sim_valid: std_logic := '0';
+    signal sim_ready: std_logic;
+    signal sim_last: std_logic := '0';
 
-    signal s_axi_data : std_logic_vector(PACKETSIZE*PIXELSIZE-1 downto 0);
-    signal s_axi_valid : std_logic;
-    signal s_axi_ready : std_logic := '1';
-    signal s_axi_last : std_logic;
+    signal module_data : std_logic_vector(PACKETSIZE*PIXELSIZE-1 downto 0);
+    signal module_valid : std_logic;
+    signal module_ready : std_logic := '1';
+    signal module_last : std_logic;
 
 begin
 
@@ -56,35 +56,35 @@ begin
         wait until clk'event and clk='1';
 
         if rst = '1' then
-            m_axi_data <= (others => '0');
-            m_axi_valid <= '0';
-            s_axi_ready <= '1';
-            m_axi_last <= '0';
+            sim_data <= (others => '0');
+            sim_valid <= '0';
+            module_ready <= '1';
+            sim_last <= '0';
 
         else
-            s_axi_ready <= not s_axi_ready;
-            if not endfile(simulationFile) and s_axi_ready = '1' then
+            module_ready <= not module_ready;
+            if not endfile(simulationFile) and module_ready = '1' then
                 readline(simulationFile, line_v);
                 hread(line_v, simulation);
-                m_axi_data <= simulation;
-                m_axi_valid <= '1';
-                m_axi_dim <= X"040040"; -- HEIGHT:WIDTH
+                sim_data <= simulation;
+                sim_valid <= '1';
+                sim_dim <= X"040040"; -- HEIGHT:WIDTH
                 if endfile(simulationFile) then
-                    m_axi_last <= '1';
+                    sim_last <= '1';
                 end if;
-            elsif endfile(simulationFile) and s_axi_ready = '1' then
-                m_axi_valid <= '0';
-                m_axi_last <= '0';
-                m_axi_dim <= (others => '0');
-                m_axi_data <= (others => '0');
+            elsif endfile(simulationFile) and module_ready = '1' then
+                sim_valid <= '0';
+                sim_last <= '0';
+                sim_dim <= (others => '0');
+                sim_data <= (others => '0');
             end if;
 
-            if not endfile(assertionFile) and s_axi_ready = '1' then
+            if not endfile(assertionFile) and module_ready = '1' then
                 readline(assertionFile, line_v2);
                 hread(line_v2, assertion);
                 assertRow <= assertion;
-                assert s_axi_data = assertRow report "Incorrect " & integer'image(to_integer(unsigned(s_axi_data))) & " /= " & integer'image(to_integer(unsigned(assertRow)));
-            elsif endfile(assertionFile) and s_axi_ready = '1' then
+                assert module_data = assertRow report "Incorrect " & integer'image(to_integer(unsigned(module_data))) & " /= " & integer'image(to_integer(unsigned(assertRow)));
+            elsif endfile(assertionFile) and module_ready = '1' then
                 assertRow <= (others => '0');
             end if;
 
@@ -99,14 +99,14 @@ begin
     port map (
         clk => clk,
         rst => rst,
-        s_axi_valid => m_axi_valid,
-        s_axi_ready => m_axi_ready,
-        s_axi_data => m_axi_data,
-        s_axi_dim => m_axi_dim,
-        s_axi_last => m_axi_last,
-        m_axi_valid => s_axi_valid,
-        m_axi_ready => s_axi_ready,
-        m_axi_last => s_axi_last,
-        m_axi_data => s_axi_data);
+        s_axi_valid => sim_valid,
+        s_axi_ready => sim_ready,
+        s_axi_data => sim_data,
+        s_axi_dim => sim_dim,
+        s_axi_last => sim_last,
+        m_axi_valid => module_valid,
+        m_axi_ready => module_ready,
+        m_axi_last => module_last,
+        m_axi_data => module_data);
 
 end architecture;
